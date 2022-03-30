6c06f01241acf1009e209971f72737b5e18b69f1
Sebastian ZL Fabricato <sezalo@hotmail.es>
2022-03-30 07:58:40 -0500

    Se cierra assigment pollution y se elimina testdirectory

--- Pollution_assigment/pollutantmean.R
+++ Pollution_assigment/pollutantmean.R
@@ -5,7 +5,7 @@ rm(list = ls())
 directorio<-getwd( ) 
 directorio<-paste0(directorio,"/Pollution_assigment/specdata/")#Añade restante del directorio, paste0 no deja espacio
 
-pollutantmean <-function(directory,pollutant,id=1:332){
+pollutantmean <-function(directory,pollutant,id=1:332){  #Promedio de contaminación por contaminante
   fileList<-list.files(path =directory,pattern = ".csv",full.names=TRUE) #Lee todos los archivos
   values<-numeric()#Preloca vector de valores de interes
     for (i in id){
@@ -16,12 +16,12 @@ pollutantmean <-function(directory,pollutant,id=1:332){
     # alocar el siguiente codigo dentro de este ciclo u<-merge(u,data,all.x = TRUE)
     values<-c(values,data[[pollutant]])
   }
-  resultado1<-mean(values,na.rm = TRUE)
+  resultado1<<-mean(values,na.rm = TRUE)
 }
 
 #pollutantmean(directorio,"nitrate",70:72)
 
-complete<-function(directory,id=1:332){
+complete<-function(directory,id=1:332){  #Cuenta las observaciones completas de cada archivo
   fileList<-list.files(path =directory,pattern = ".csv",full.names=TRUE) #Lee todos los archivos
   resultado<-data.frame(id=numeric(0),nobs=numeric(0)) # Reloca DF de resultados
   for(i in id){
@@ -37,7 +37,7 @@ complete<-function(directory,id=1:332){
 
 #complete(directorio, c(2, 4, 8, 10, 12))
 
-corr<-function(directory,threshold=0){
+corr<-function(directory,threshold=0){ #Cuando se supera un numero de archivos con datos completos, se saca correlacion de nitrato y sulfato entre los datos completos de cada archivo
   cor_results<-numeric(0) #Realoca
   complete_cases<-complete(directory) #Mira que corridas tienen todos los datos
   
@@ -50,8 +50,10 @@ corr<-function(directory,threshold=0){
       sulf_data<-df["sulfate"]
       nit_data<-df["nitrate"]
       cor_results<-c(cor_results,cor(sulf_data,nit_data))
-      test<<-cor(sulf_data,nit_data)
+      test<<-cor(sulf_data,nit_data) 
       }
   }
-  resultado3<<-cor_results
+  resultado3<<-cor_results #Si no se pone return(cor_results) entrega por defecto la ultima definicion al llamar x<-complete(argumentos)
 }
+#cr <- corr(directorio, 400) #
+#summary(cr) #Da (min, Q1, media, promedio, Q3 y max) de los datos en cr
