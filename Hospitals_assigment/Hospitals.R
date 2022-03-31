36cab2c3f0e99a3eb00e954a5b6a830c40a4ba45
Sebastian ZL Fabricato <sezalo@hotmail.es>
2022-03-31 14:34:20 -0500

    Añade documentos

--- Hospitals_assigment/Hospitals.R
+++ Hospitals_assigment/Hospitals.R
@@ -1,3 +1,27 @@
 cat("\014") 
 rm(list = ls())
 
+#CCARGA DATOS
+directorio<-getwd( ) 
+outcome_path<-paste0(directorio,"/Hospitals_assigment/outcome-of-care-measures.csv")
+hospitals_path<-paste0(directorio,"/Hospitals_assigment/hospital-data.csv")
+outcome <- read.csv(outcome_path)
+hospitals<-read.csv(hospitals_path)
+
+#PLOT
+outcome[, 11] <- as.numeric(outcome[, 11]) # Coniverte rata de ataque cardiacos
+hist(outcome[, 11],xlab='Muertes',main='Frecuencia de ratas mensuales de mortalidad por ataque cardiaco en hospitales')
+
+#MEJOR HOSPITAL EN EL ESTADO
+best<-function(state= character(),disease=character()){
+  return(state)
+  
+  if(!is.null(inv)) {
+    message("getting cached data")
+    return(inv)}
+  
+  if(!is.null(inv)) {
+    message("getting cached data")
+    return(inv)}
+  
+}
