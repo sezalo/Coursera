180301180e8e52ee9f0a3ad1a7b1d9778ff1315f
Sebastian ZL Fabricato <sezalo@hotmail.es>
2022-03-31 19:53:06 -0500

    Avances en segundo punto

--- Hospitals_assigment/Hospitals.R
+++ Hospitals_assigment/Hospitals.R
@@ -1,27 +1,100 @@
 cat("\014") 
 rm(list = ls())
 
-#CCARGA DATOS
+
+
+##CARGA DATOS
 directorio<-getwd( ) 
 outcome_path<-paste0(directorio,"/Hospitals_assigment/outcome-of-care-measures.csv")
 hospitals_path<-paste0(directorio,"/Hospitals_assigment/hospital-data.csv")
 outcome <- read.csv(outcome_path)
 hospitals<-read.csv(hospitals_path)
 
-#PLOT
+
+
+##PLOT
 outcome[, 11] <- as.numeric(outcome[, 11]) # Coniverte rata de ataque cardiacos
 hist(outcome[, 11],xlab='Muertes',main='Frecuencia de ratas mensuales de mortalidad por ataque cardiaco en hospitales')
 
-#MEJOR HOSPITAL EN EL ESTADO
+
+
+##MEJOR HOSPITAL EN EL ESTADO
 best<-function(state= character(),disease=character()){
-  return(state)
   
-  if(!is.null(inv)) {
-    message("getting cached data")
-    return(inv)}
+  #Datos de interes
+  ratas <- as.data.frame(cbind(outcome[, 2],  # hospital 
+                               outcome[, 7],   # estado
+                               outcome[, 11],  # corataq: ataque al corazon
+                               outcome[, 17],  # corafa: falla del corazon
+                               outcome[, 23]), # pneumonia
+                         stringsAsFactors = FALSE) #Evita conversion de numericos a factor al unir con cbind
+  #Renombra las columnas
+  colnames(ratas)<-c("hospital", "state", "heart attack", "heart failure", "pneumonia")
   
-  if(!is.null(inv)) {
-    message("getting cached data")
-    return(inv)}
+  #Testea los entradas sumistradas
+  if(!state %in% ratas[,"state"]){
+    stop('invalid state')
+  }
+  
+  if(!disease %in% c("heart attack", "heart failure", "pneumonia")){
+    stop('invalid outcome')
+  }
+  
+  ratas<-ratas[ratas[,"state"]==state,] #Exluye hospitales del estado de interes
+  ratas[,disease]=as.numeric(ratas[,disease]) #Cambia de character a numeric
+  ratas<-ratas[!is.na(ratas[,disease]),] #Elimina los NA
+  ratas<-ratas[order(ratas[, disease]), ] #Organiza por orden, arriba quedan los puntajes mas bajas pero mejores
+  
+  Hosp <- ratas[ratas[, disease] == min(ratas[,disease]),1] #Obtiene los nombres que tienen el menor puntaje pero mejor
+  sort(Hosp)[1]
+}
+    #best("TX", "heart attack") # e.g
+    #best("MD", "pneumonia") #e.g
+
+
+
+## OBTIENE EL HOSPITAL EN DETERMINADA POSICION DEL RANKING
+rankhospital<-function(state= character(),disease=character(),rank="best"){
+  
+  #Datos de interes
+  ratas <- as.data.frame(cbind(outcome[, 2],  # hospital 
+                               outcome[, 7],   # estado
+                               outcome[, 11],  # corataq: ataque al corazon
+                               outcome[, 17],  # corafa: falla del corazon
+                               outcome[, 23]), # pneumonia
+                         stringsAsFactors = FALSE) #Evita conversion de numericos a factor al unir con cbind
+  #Renombra las columnas
+  colnames(ratas)<-c("hospital", "state", "heart attack", "heart failure", "pneumonia")
+  
+  #Testea los entradas sumistradas 
+  if(!state %in% ratas[,"state"]){
+    stop('invalid state')
+  }
+  
+  if(!disease %in% c("heart attack", "heart failure", "pneumonia")){
+    stop('invalid outcome')
+  }
+  
+  ratas<-ratas[ratas[,"state"]==state,] #Exluye hospitales del estado de interes
+  ratas[,disease]=as.numeric(ratas[,disease]) #Cambia de character a numeric
+  ratas<-ratas[!is.na(ratas[,disease]),] #Elimina los NA
+  
+  #Convierte la entrada de posicion a numerica y evalua si la posicion es valida
+  num<-rank
+  if(rank == "best") {
+    num <- 1 
+  }
+  
+  if (rank == "worst") {
+    num <<- length(ratas[,1])
+    #num<<-rank
+  }
+  
+  if (length(ratas[,1])<rank){
+    return(NA)
+  }
+
+  ratas<<-ratas[order(ratas[, disease]), ] #Organiza por orden, arriba quedan los puntajes mas bajas pero mejores
+  ratas[num,1] #Obtiene el hospital en la posicion
   
 }
