25f41592e45b157212dd77171c27bb167e144d8b
Sebastian ZL Fabricato <sezalo@hotmail.es>
2022-04-04 14:13:38 -0500

    Finalizado

--- DataExploration_assigment/plot6.R
+++ DataExploration_assigment/plot6.R
@@ -1,5 +1,6 @@
 rm(list = ls())
 cat("\014") 
+library(ggplot2)
 
 ##CARGA DATOS
 directorio<-getwd()
@@ -12,13 +13,19 @@ SCC <- readRDS(rutas[1])
 indice<-grepl("vehicle",SCC$SCC.Level.Two,ignore.case=TRUE)#Vector logico que indica si vehicle aparece en en los nomrbre de SCC$Short
 vehiculos<-SCC[indice,] #Toma entradas correspondiente a vehiculos
 
-##EXTRAE
+##EXTRAE POR CIUDAD
 BaltData<-subset(NEI,fips=="24510")
+BaltData$city <- "Baltimore City"
+LAData<-subset(NEI,fips=="06037")
+LAData$city <- "Los Angeles Country"
 
 ##JUNTA
-completo<-merge(BaltData,vehiculos, by="SCC")
-xyear<-tapply(BaltData$Emissions,BaltData$year,sum)
+BaltData<-merge(BaltData,vehiculos, by="SCC")
+LAData<-merge(LAData,vehiculos, by="SCC")
+completo<-rbind(BaltData,LAData)
+r2plot<-aggregate(Emissions~year+city,completo,sum)
 
 ##GRAFICA
-barplot(xyear, xlab = "Year", ylab = "Total Emissions [Ton]", 
-        main = "Baltimore´s yearly total emission from motor sources.")
+g<- ggplot(r2plot, aes(year,Emissions,color=city))
+g + geom_line()+xlab("Year")+ylab(expression("Total PM"[2.5]*" Emissions")) +
+  ggtitle("Baltimore´s and LA´s total motor sources emissions")
